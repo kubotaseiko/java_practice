14a2a893e8523536c273dd9483ba77ea0839d49c
kubotaseiko <kubota712.050@gmail.com>
2021-08-04 13:45:48 +0900

    First commit

---
+++ src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String myname = "seiko";
+		System.out.println(myname);
+		
+		myname = "美味しい";
+		System.out.println(myname);
+		
+		int number = 13;
+		System.out.println(number);
+	}
+
+}
---
+++ src/Chapter05.java
@@ -0,0 +1,24 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 111;
+		double double1 = 22.2;
+		boolean boolean1 = true;
+		Float aaa = 10.1f;
+		Integer bbb = 222;
+		Character ccc = 'あ';
+		int [] int01 = new int[5];
+		int01[0] = 9;
+		int01[1] = 8;
+		int01[2] = 7;
+		int01[3] = 6;
+		int01[4] = 5;
+		
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(aaa);
+		System.out.println(bbb);
+		System.out.println(ccc);
+		System.out.println(3);
+	}
+}
