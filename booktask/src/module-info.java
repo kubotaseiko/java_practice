bb973cd4737fe949556c9e55c39ef3db2e31378b
kubotaseiko <kubota712.050@gmail.com>
2021-08-16 19:29:45 +0900

    task0816

---
+++ booktask/src/module-info.java
@@ -0,0 +1,2 @@
+module booktask {
+}
---
+++ booktask/src/task/Main.java
@@ -0,0 +1,12 @@
+package task;
+import comment.Zenhan;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		comment.Kouhan.callDeae();
+		comment.Kouhan.showMondakoro();
+	}
+
+}
