f1bc02827faa500ed464acac49aa37ab4a709b57
kubotaseiko <kubota712.050@gmail.com>
2021-08-21 23:16:05 +0900

    practice0821

---
+++ booktask/src/task10_2/Hero.java
@@ -0,0 +1,6 @@
+package task10_2;
+
+public class Hero {
+	String name;
+	int hp;
+}
---
+++ booktask/src/task9_3/Main.java
@@ -0,0 +1,21 @@
+package task9_3;
+
+public class Main {
+	
+	public static void heal(int hp) {
+		hp += 10;
+	}
+	
+	public static void heal(Thief thief){
+		thief.hp += 10;
+	}
+	
+	public static void main(String[] args) {
+		int baseHp = 25;
+		Thief t = new Thief("ÉAÉTÉJ", baseHp);
+		System.out.println(baseHp + " : " + t.hp);
+		heal(baseHp);
+		heal(t);
+		System.out.println(baseHp + " : " + t.hp);
+	}
+}
