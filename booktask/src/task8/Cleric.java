2c1e7a87413a303f14d00d519c12fa838b1bb710
kubotaseiko <kubota712.050@gmail.com>
2021-08-26 15:01:45 +0900

    practice0826(API)

--- booktask/src/task8/Cleric.java
+++ booktask/src/task8/Cleric.java
@@ -3,9 +3,9 @@ package task8;
 public class Cleric {
 	String name;
 	int hp = 50;
-	final int MAXHP = 50;
+	static final int MAXHP = 50;
 	int mp = 10;
-	final int MAXMP = 10;
+	static final int MAXMP = 10;
 
 	public void selfAid() {
 		this.mp -= 5;
